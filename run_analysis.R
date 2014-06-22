cb8981ea19f255b955db2213350c5c0aed7504df
Gabriel Armelin <gabrielbma@gmail.com>
2014-06-22 17:17:26 -0400

    improved run_analysis.R

---
+++ run_analysis.R
@@ -0,0 +1,40 @@
+x_test_dataset <- read.table("test/X_test.txt", sep="\n", colClasses=c("character"), 
+#                            nrows=3,
+                             strip.white = F, blank.lines.skip = T)
+
+x_train_dataset <- read.table("test/X_test.txt", sep="\n", colClasses=c("character"),
+#                            nrows=3,
+                             strip.white = F, blank.lines.skip = T)
+
+x_dataset <- rbind(x_test_dataset, x_train_dataset)
+
+x_dataset$id <- seq(1:nrow(x_dataset))
+
+x_dataset$Mean <- sapply(x_dataset$V1, function(data){
+  d <- as.numeric(unlist(strsplit(data,"\\s+")))
+  mean(d, na.rm=T)
+})
+
+x_dataset$StandardDeviation <- sapply(x_dataset$V1, function(data){
+  d <- as.numeric(unlist(strsplit(data,"\\s+")))
+  sd(d, na.rm=T)
+})
+
+y_test_dataset <- read.table("test/Y_test.txt", sep="\n")
+y_train_dataset <- read.table("train/Y_train.txt", sep="\n")
+
+y_dataset <- rbind(y_test_dataset, y_train_dataset)
+
+y_dataset$id <- seq(1:nrow(y_dataset))
+
+tidyDataset1 <- merge(x_dataset, y_dataset, by="id")
+tidyDataset1$ActivityName <- factor(tidyDataset1$V1.y, 
+                              levels=c(1,2,3,4,5,6), 
+                              labels=c("WALKING","WALKING_UPSTAIRS",
+                                       "WALKING_DOWNSTAIRS","SITTING",
+                                       "STANDING","LAYING"))
+
+tidyDataset1$V1.x <- NULL
+tidyDataset1$V1.y <- NULL
+write.table(tidyDataset1, "tidydataset1.txt", sep=";", row.names=F)
+
