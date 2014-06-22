78388fc19bbc5433b79cf3ac015b12cc64b27beb
Gabriel Armelin <gabrielbma@gmail.com>
2014-06-22 17:50:26 -0400

    updated run_analysis.r

--- run_analysis.R
+++ run_analysis.R
@@ -1,13 +1,12 @@
 x_test_dataset <- read.table("test/X_test.txt", sep="\n", colClasses=c("character"), 
-#                            nrows=3,
+#                            nrows=numberOfLines,
                              strip.white = F, blank.lines.skip = T)
 
-x_train_dataset <- read.table("test/X_test.txt", sep="\n", colClasses=c("character"),
-#                            nrows=3,
+x_train_dataset <- read.table("train/X_train.txt", sep="\n", colClasses=c("character"),
+#                            nrows=numberOfLines,
                              strip.white = F, blank.lines.skip = T)
 
 x_dataset <- rbind(x_test_dataset, x_train_dataset)
-
 x_dataset$id <- seq(1:nrow(x_dataset))
 
 x_dataset$Mean <- sapply(x_dataset$V1, function(data){
@@ -22,10 +21,14 @@ x_dataset$StandardDeviation <- sapply(x_dataset$V1, function(data){
 
 y_test_dataset <- read.table("test/Y_test.txt", sep="\n")
 y_train_dataset <- read.table("train/Y_train.txt", sep="\n")
-
 y_dataset <- rbind(y_test_dataset, y_train_dataset)
-
 y_dataset$id <- seq(1:nrow(y_dataset))
+
+test_subject <- read.table("test/subject_test.txt", sep="\n",colClasses=c("numeric"))
+train_subject <- read.table("train/subject_train.txt", sep="\n", colClasses=c("numeric"))
+
+subject <- rbind(test_subject, train_subject)
+subject$id <- seq(1:nrow(subject))
 
 tidyDataset1 <- merge(x_dataset, y_dataset, by="id")
 tidyDataset1$ActivityName <- factor(tidyDataset1$V1.y, 
